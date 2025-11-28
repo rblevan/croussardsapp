6467550e654d1662d26c33737d2228e135ca2994
Stephane <stephane.castan@outlook.fr>
2025-11-28 09:15:50 +0100

    save

--- croussardsapi/src/main/java/fr/univpoitiers/croussardsapi/dto/RegisterRequest.java
+++ croussardsapi/src/main/java/fr/univpoitiers/croussardsapi/dto/RegisterRequest.java
@@ -3,7 +3,7 @@ package fr.univ_poitiers.croussards.dto;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class .RegisterRequest {
     private String name;
     private String first_name;
     private String mail;
